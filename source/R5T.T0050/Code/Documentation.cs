57f8af0e8453c062c7804d1401b731f53fa6ef69
davidcoats <david@getsafetycone.com>
2021-09-27 14:47:11 -0700

    Initial commit of source-stub.

---
+++ source/R5T.T0050/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0050
+{
+    /// <summary>
+    /// Code file path, C# code file path, and other strongly-typed string types.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
