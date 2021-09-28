bb84a6b26bad0f11ef5087437ebe134bf0ed95ce
David Coats <david@getsafetycone.com>
2021-09-28 15:27:28 -0700

    Add CSharpCodeFilePath strongly-typed string type.

--- source/R5T.T0050/Code/Classes/Class1.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-
-namespace R5T.T0050
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.T0050/Code/Strong Types/CSharpCodeFilePath.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using R5T.Stagira;
+
+
+namespace R5T.T0050
+{
+    public class CSharpCodeFilePath : TypedString
+    {
+        #region Static
+
+        public static CSharpCodeFilePath From(string value)
+        {
+            var output = new CSharpCodeFilePath(value);
+            return output;
+        }
+
+        #endregion
+
+
+        public CSharpCodeFilePath(string value)
+            : base(value)
+        {
+        }
+    }
+}
